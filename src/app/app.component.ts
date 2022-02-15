234334e28a036593448e00eabbe43011cc7c401b
robint <nakim@gmx.fr>
2022-02-15 19:18:29 +0100

    Strucurez un document avec des directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,32 +7,33 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  mySnap!: FaceSnap;
-  myOtherSnap!: FaceSnap;
-  myLastSnap!: FaceSnap;
+  faceSnaps!: FaceSnap[];
+
 
   ngOnInit() {
-    this.mySnap = {
-      title: 'Archibald',
-      description: 'Mon meilleur ami depuis tout petit !',
-      imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      createdDate: new Date(),
-      snaps: 0
-  } ;    
-    this.myOtherSnap = {
-      title:' ThreeRockMontain',
-      description: 'Mon meilleur ami depuis tout petit !',
-      imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      createdDate: new Date(),
-      snaps: 0,
-      location: 'mon Jardin'
-    };    
-    this.myLastSnap = {
-      title: 'UnBonRepas',
-      description: 'Mon meilleur ami depuis tout petit !',
-      imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      createdDate: new Date(),
-      snaps: 0
-    };
+    this.faceSnaps = [
+      {
+        title: 'Archibald',
+        description: 'Mon meilleur ami depuis tout petit !',
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        createdDate: new Date(),
+        snaps: 240
+      },    
+      {
+        title:' ThreeRockMontain',
+        description: 'Mon meilleur ami depuis tout petit !',
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        createdDate: new Date(),
+        snaps: 0,
+        location: 'mon Jardin'
+      },    
+      {
+        title: 'UnBonRepas',
+        description: 'Mon meilleur ami depuis tout petit !',
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        createdDate: new Date(),
+        snaps: 0
+      }
+    ];
   }
 }
